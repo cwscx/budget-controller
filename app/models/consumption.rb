20563404b597a8ef4b322d1dcdb750def56a7734
Shengyang Shi <shengyang.shi@hotmail.com>
2015-07-20 20:27:38 +0800

    login/logout

--- app/models/consumption.rb
+++ app/models/consumption.rb
@@ -1,9 +1,5 @@
 class Consumption < ActiveRecord::Base
   belongs_to :user
-  validates_presense_of :name
-  validates_presense_of :price
-  validates_presense_of :date
   
   validates_numericality_of :price
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-         has_many :consumptions
+  has_many :consumptions
+  
+  def hasConsumptions?
+    @all_consumptions = consumptions.find_by_user_id(self.id)
+    if(@all_consumptions.nil?)
+      false
+    else
+      @all_consumptions
+    end
+  end
+  
 end
